e17b1f11b42fb58419eb92636983432e4808dd60
klebersuarez <36607956+klebersuarez@users.noreply.github.com>
2020-04-19 09:14:54 -0500

    Revert "🚧 work"
    This reverts commit 3efad317c572fb636e939200c9fd48d759c5dbd6.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
